0e52999e72c874dad61b127ef8f717c0d10b975e
Alutta <Aluttamatambo5@gmail.com>
2024-11-03 14:51:47 +0200

    added css styling and image rendering

--- PreviewComponent.js
+++ PreviewComponent.js
@@ -12,3 +12,36 @@ class BookPreview extends HTMLElement {
     attributeChangedCallback(name, oldValue, newValue) {
         this.render(); // Re-render when attributes change
     }
+    render() {
+        const image = this.getAttribute('data-image');
+        const title = this.getAttribute('data-title');
+        const author = this.getAttribute('data-author');
+        const id = this.getAttribute('data-preview');
+
+        this.shadowRoot.innerHTML = `
+            <style>
+                .book-preview {
+                    border: 1px solid #ccc;
+                    padding: 10px;
+                    margin: 10px;
+                    display: inline-block;
+                    cursor: pointer;
+                }
+                img {
+                    max-width: 100px;
+                }
+            </style>
+            <div class="book-preview" data-preview="${id}">
+                <img src="${image}" alt="${title}" />
+                <h3>${title}</h3>
+                <p>${author}</p>
+            </div>
+        `;
+
+        this.shadowRoot.querySelector('.book-preview').addEventListener('click', () => {
+            this.dispatchEvent(new CustomEvent('book-select', { detail: id, bubbles: true }));
+        });
+    }
+}
+
+customElements.define('book-preview', BookPreview);
